aebdc26368e0ad53c0347e6d79da99d1b8b34006
dishavishnani <mpdishavishnani00170@gmail.com>
2026-02-25 21:34:25 +0530

    simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,13 @@
+
+echo "Enter Principal:"
+read principal
+
+echo "Enter Rate of Interest:"
+read rate
+
+echo "Enter Time Period:"
+read time
+
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+
+echo "Simple Interest is: $interest"
